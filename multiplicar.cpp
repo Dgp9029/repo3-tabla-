0b4e5c10ceeec845541ce67cc44298fca63ae2ff
Dante Pereira <dante.pereira0815@gmail.com>
2024-04-17 16:58:21 +0200

    tabla multiplicar

---
+++ multiplicar.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+    using namespace std;
+
+    int main()
+
+   {
+       int numerousu;
+
+
+       cout<<"ingrese el numero"<<endl;
+       cin>>numerousu;
+
+       for(int i= 1; i<=10; i++)
+
+       cout<<numerousu *i<< endl;
+
+
+
+       return 0;
+   }
